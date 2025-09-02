a47bef99e47e5782a0a41e692ab00a30ec0672f0
Christopher Alexan <chrisalexan123@yahoo.com>
2025-09-02 16:10:47 -0700

    attempting to intsall packages that help do swift on windows

--- MyCLI/Hello.swift
+++ MyCLI/Hello.swift
@@ -1,5 +1,9 @@
  //an up to date import for swift
 
+
+
+
+
 let firstGreeting = "Hello World" //creates variable firstGreeting and attatches value
 
 print(firstGreeting) //displays Hello World to the terminal
