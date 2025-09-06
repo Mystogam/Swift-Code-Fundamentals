830108a8c9842825cf239868209894d3d6f9a80b
Christopher Alexan <chrisalexan123@yahoo.com>
2025-09-06 01:07:38 -0700

    made a mistake in the printing again that is now been corrected so that it uses the singular and not plural form of the variable so that it doesnt print the entire array.

--- Loops.swift
+++ Loops.swift
@@ -4,5 +4,5 @@ let rulersNames = ["Alexander", "Julius", "Peter", "Catherine", "Saladin"] //cre
 
 for ruler in rulersNames //iterates through the value of rulersNames and displays it below, use ruler instead of underscore so that it doesnt dump the entire array  
 {
-    print("Greatest rulers in history: ", rulersNames) //requires comma in between what is needed to be printed out so that it could tell the difference
+    print("Greatest rulers in history: ", ruler) //requires comma in between what is needed to be printed out so that it could tell the difference, also ensure that the use a variable is the singular one when trying to print out a singular value
 }
