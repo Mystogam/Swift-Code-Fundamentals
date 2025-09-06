bf2765d4d9f5418ff5922ec348b83fd86887990f
Christopher Alexan <chrisalexan123@yahoo.com>
2025-09-06 01:02:27 -0700

    fixing a problem with for loop that displayed the entire array and displayed it 4 times, including adding comments that better describe the code.

--- Loops.swift
+++ Loops.swift
@@ -2,7 +2,7 @@ import Foundation //calling for library
 
 let rulersNames = ["Alexander", "Julius", "Peter", "Catherine", "Saladin"] //creating variable of rulers name to loop
 
-for _ in rulersNames //iterates through the value of rulersNames and displays it below
+for ruler in rulersNames //iterates through the value of rulersNames and displays it below, use ruler instead of underscore so that it doesnt dump the entire array  
 {
-    print("Greatest rulers in history ", rulersNames)  
+    print("Greatest rulers in history: ", rulersNames) //requires comma in between what is needed to be printed out so that it could tell the difference
 }
