497130c4d2e1c8b636c08a68b1ce5bce1e049156
Christopher Alexan <chrisalexan123@yahoo.com>
2025-09-06 14:47:33 -0700

    using the ridiculously long if else statement chain to print out different sentences for each coresponding rulers.

--- moreLoops.swift
+++ moreLoops.swift
@@ -2,7 +2,7 @@ import Foundation
 
 let rulersNames: [String] = ["Alexander", "Napoleon", "Julius Ceasar", "Hannibal", "George Washington"]
 
-for ruler in rulersNames
+for ruler in rulersNames //iterates through the rulersNames string values
 {
     if(ruler == "Alexander")
     {
@@ -10,18 +10,18 @@ for ruler in rulersNames
     }
     else if(ruler == "Napoleon")
     {
-        print(ruler, "")
+        print(ruler, "is the greatest general to ever live.")
     }
     else if(ruler == "Julius Ceaser")
     {
-
+        print(ruler, "is one of Rome's greatest rulers and helped create the Roman Empire.")
     }
     else if(ruler == "Hannibal")
     {
-        
+        print(ruler, "is one of the greatest minds for strategy to ever step on the field of battle.")
     }
     else if(ruler == "George Washington")
     {
-
+        print(ruler, "is the first American President.")
     }
 }
