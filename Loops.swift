3589c6b9e1cee1dae0e5fefe81a07a5a80ee38e7
Christopher Alexan <chrisalexan123@yahoo.com>
2025-09-05 20:37:23 -0700

    creating the basis of my for loop to loop through the different leaders names

--- Loops.swift
+++ Loops.swift
@@ -1,3 +1,8 @@
 import Foundation //calling for library
 
 let rulersNames = ["Alexander", "Julius", "Peter", "Catherine", "Saladin"] //creating variable of rulers name to loop
+
+for _ in rulersNames
+{
+    
+}
